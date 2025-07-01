6956ccfdedaf84a29ba8b4df47b8ff507ae9e1bf
Emmanuel C <eechukwu@gmail.com>
2025-07-01 12:24:27 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,34 @@
-module "sandbox" {
+module "service_quota_test_account" {
   source = "./modules/aft-account-request"
-
+  
   control_tower_parameters = {
-    AccountEmail              = "eechukwuawslab+sandboxaft@gmail.com"
-    AccountName               = "sandbox-aft-2"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "eechukwuawslab+sandboxaft-2@gmail.com"
-    SSOUserFirstName          = "eechukwuawslab+sandboxaft-2"
-    SSOUserLastName           = "eechukwuawslab2"
+    AccountEmail              = "eechukwuawslab+quotatest@gmail.com"
+    AccountName               = "quota-test-aft-sg-rules"
+    ManagedOrganizationalUnit = "Service-Quota-Testing"
+    SSOUserEmail              = "eechukwuawslab+quotatest@gmail.com"
+    SSOUserFirstName          = "ServiceQuota"
+    SSOUserLastName           = "TestUser"
   }
-
+  
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Purpose"           = "Service-Quota-Automation-Testing"
+    "Project"           = "AFT-SecurityGroup-Quota"
+    "Environment"       = "Test"
+    "AutomatedQuotas"   = "SecurityGroupRules-200"
+    "Learn Tutorial"    = "AFT"
   }
-
+  
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "AFT Service Quota Implementation Team"
+    change_reason       = "Testing automated Security Group rules quota increase from 60 to 200"
   }
-
+  
   custom_fields = {
-    group = "non-prod"
+    group                = "quota-testing"
+    expected_sg_quota    = "200"
+    quota_automation     = "enabled"
+    test_purpose         = "service-quota-validation"
   }
-
-  account_customizations_name = "sandbox"
+  
+  account_customizations_name = "service-quota-test"
 }
