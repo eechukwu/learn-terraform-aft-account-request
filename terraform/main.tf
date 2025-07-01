e9267414d0ec249a7e533a1f04749537961720e5
Emmanuel C <eechukwu@gmail.com>
2025-07-01 14:57:15 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,92 +1,27 @@
-module "service_quota_test" {
+module "sandbox" {
   source = "./modules/aft-account-request"
-  
+
   control_tower_parameters = {
-    AccountEmail              = "eechukwuawslab+quotatest@gmail.com"
-    AccountName               = "quota-test-sg-rules"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "eechukwuawslab+quotatest@gmail.com"
-    SSOUserFirstName          = "QuotaTest"
-    SSOUserLastName           = "User"
+    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
   }
-  
+
   account_tags = {
     "Learn Tutorial" = "AFT"
-    "Purpose"        = "Service-Quota-Testing"
-    "QuotaTarget"    = "SG-Rules-200"
   }
-  
-  change_management_parameters = {
-    change_requested_by = "Service Quota Implementation"
-    change_reason       = "Test automated Security Group quota increase"
-  }
-  
-  custom_fields = {
-    group = "quota-test"
-  }
-  
-  account_customizations_name = "service-quota-test"
-}
 
-
-# Test Account 1 - Service Quota Testing
-module "quota_test_account_1" {
-  source = "./modules/aft-account-request"
-  
-  control_tower_parameters = {
-    AccountEmail              = "eechukwuawslab+quotatest1@gmail.com"
-    AccountName               = "quota-test-sg-1"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "eechukwuawslab+quotatest1@gmail.com"
-    SSOUserFirstName          = "QuotaTest"
-    SSOUserLastName           = "User1"
-  }
-  
-  account_tags = {
-    "Purpose"        = "Service-Quota-Testing"
-    "TestNumber"     = "1"
-    "ExpectedQuota"  = "200"
-  }
-  
   change_management_parameters = {
-    change_requested_by = "AFT Service Quota Test"
-    change_reason       = "Test automated Security Group quota increase - Account 1"
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
-  
-  custom_fields = {
-    group = "quota-test-1"
-  }
-  
-  account_customizations_name = "quota-test-1"
-}
 
-# Test Account 2 - Service Quota Testing
-module "quota_test_account_2" {
-  source = "./modules/aft-account-request"
-  
-  control_tower_parameters = {
-    AccountEmail              = "eechukwuawslab+quotatest2@gmail.com"
-    AccountName               = "quota-test-sg-2"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "eechukwuawslab+quotatest2@gmail.com"
-    SSOUserFirstName          = "QuotaTest"
-    SSOUserLastName           = "User2"
-  }
-  
-  account_tags = {
-    "Purpose"        = "Service-Quota-Testing"
-    "TestNumber"     = "2"
-    "ExpectedQuota"  = "200"
-  }
-  
-  change_management_parameters = {
-    change_requested_by = "AFT Service Quota Test"
-    change_reason       = "Test automated Security Group quota increase - Account 2"
-  }
-  
   custom_fields = {
-    group = "quota-test-2"
+    group = "non-prod"
   }
-  
-  account_customizations_name = "quota-test-2"
+
+  account_customizations_name = "sandbox"
 }
